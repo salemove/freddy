197eac8a102f747a9a7ad0c70458778769c7c907
Indrek Juhkam <indrek@urgas.eu>
2016-01-20 17:26:43 +0200

    Add response logger to tap into consumer

--- lib/freddy/consumers/tap_into_consumer.rb
+++ lib/freddy/consumers/tap_into_consumer.rb
@@ -1,7 +1,10 @@
+require_relative 'consumer'
+
 class Freddy
   module Consumers
     class TapIntoConsumer
-      def initialize(consume_thread_pool)
+      def initialize(consume_thread_pool, logger)
+        @logger = logger
         @consume_thread_pool = consume_thread_pool
       end
 
@@ -9,7 +12,7 @@ class Freddy
         queue = create_queue(pattern, channel)
 
         consumer = queue.subscribe do |delivery|
-          process_message(delivery, &block)
+          process_message(queue, delivery, &block)
         end
 
         ResponderHandler.new(consumer, @consume_thread_pool)
@@ -25,9 +28,18 @@ class Freddy
           .bind(topic_exchange, routing_key: pattern)
       end
 
-      def process_message(delivery, &block)
+      def process_message(queue, delivery, &block)
         @consume_thread_pool.process do
+          log_receive_event(queue.name, delivery)
           block.call delivery.payload, delivery.routing_key
+        end
+      end
+
+      def log_receive_event(queue_name, delivery)
+        if defined?(Logasm) && @logger.is_a?(Logasm)
+          @logger.debug "Received message", queue: queue_name, payload: delivery.payload, correlation_id: delivery.correlation_id
+        else
+          @logger.debug "Received message on #{queue_name} with payload #{delivery.payload} with correlation_id #{delivery.correlation_id}"
         end
       end
     end
